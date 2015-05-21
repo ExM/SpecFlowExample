f2430962a63077f8c5b54320140585ef249a28c7
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-05-21 11:07:11 +0500

    Created new project. Added references for SpecFlow, NUnit, Selenium WebDriver packages.

---
+++ SpecFlowExample/Features/Search.feature
@@ -0,0 +1,10 @@
+﻿Feature: Site search
+	Each user should be able to quickly find information on the site.
+	To do this, he can always use the search function implemented in the header.	
+
+@search
+Scenario: Add two numbers
+	Given User open global web site
+	And fill Search field with query "Languages and Dictionaries"
+	When User press Search button
+	Then the search results should be on the screen
---
+++ SpecFlowExample/StepsDefinitions/SiteSearchSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using SpecFlowExample.StepsDefinitions;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowExample.features.steps_definitions
+{
+
+    [Binding]
+    public class SiteSearchSteps : BaseStepDefinitions
+    {
+        [Given(@"User open global web site")]
+        public void GivenUserOpenGlobalWebSite()
+        {
+            BeforeScenario();
+        }
+
+        [Given(@"fill Search field with query ""(.*)""")]
+        public void GivenFillSearchFieldWithQuery(string query)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"User press Search button")]
+        public void WhenUserPressSearchButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the search results should be on the screen")]
+        public void ThenTheSearchResultsShouldBeOnTheScreen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
