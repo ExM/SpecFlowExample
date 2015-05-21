4e603ab419b07e667df36ecfc21a9f08cfd22d2e
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-05-21 13:38:20 +0500

    Added a very simple search test. Used specflow and chromedriver.

---
+++ SpecFlowExample/Features/StepDefinitions/SiteSearchSteps.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowExample.features.steps_definitions
+{
+    [Binding]
+    public class SiteSearchSteps
+    {
+        private IWebDriver driver;
+
+        [BeforeScenario]
+        public void InitScenario()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void TearDownScenario()
+        {
+            driver.Dispose();
+        }
+
+        [Given(@"User open global web site")]
+        public void GivenUserOpenGlobalWebSite()
+        {
+
+            driver.Url = "http://www.abbyy.com";
+        }
+
+        [Given(@"fill Search field with query ""(.*)""")]
+        public void GivenFillSearchFieldWithQuery(string query)
+        {
+            driver.FindElement(By.Name("searchText")).SendKeys(query);
+        }
+
+        [When(@"User press Search button")]
+        public void WhenUserPressSearchButton()
+        {
+            driver.FindElement(By.ClassName("header-search-button")).Click();
+        }
+
+        [Then(@"the search results should be on the screen")]
+        public void ThenTheSearchResultsShouldBeOnTheScreen()
+        {
+            driver.FindElement(By.XPath("//h1[text()='Search results']"));
+        }
+    }
+}
